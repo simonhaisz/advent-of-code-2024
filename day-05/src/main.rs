4938296d9c1d77cafa991eccf892191a1092b0ae
Simon Haisz <simonhaisz@gmail.com>
2024-12-05 20:13:01 -0500

    day 5 part 1

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,5 +1,29 @@
+use page_order::PageOrdering;
+use utils::{part_selection, Part, Timer};
+
 mod page_order;
 
 fn main() {
-    println!("Hello, world!");
+    let _timer = Timer::new();
+
+    let text = std::fs::read_to_string("./day-05/input.txt").unwrap();
+
+    let part = part_selection();
+
+    match part {
+        Part::One => part_1(&text),
+        Part::Two => part_2(&text),
+    }
+}
+
+fn part_1(input: &str) {
+    let page_ordering = PageOrdering::from(input);
+
+    let (_, count) = page_ordering.check_order_printing();
+
+    println!("{count}");
+}
+
+fn part_2(_input: &str) {
+    unimplemented!()
 }
--- day-05/src/page_order.rs
+++ day-05/src/page_order.rs
@@ -1,31 +1,197 @@
-#[derive(Default)]
-pub struct PageOrder {
-    order_rules: Vec<u32>,
+pub struct PageOrderRule(pub u32, pub u32);
+
+pub type Printing = Vec<u32>;
+
+trait ValidateRules<Rule> {
+    fn validate(&self, rules: &[Rule]) -> bool;
 }
 
-impl PageOrder {
-    pub fn add_rule_line(&mut self, line: &str) {
+impl ValidateRules<PageOrderRule> for Printing {
+    fn validate(&self, rules: &[PageOrderRule]) -> bool {
+        for rule in rules {
+            let left_index = self.iter().position(|&p| p == rule.0);
+            let right_index = self.iter().position(|&p| p == rule.1);
+
+            if left_index.is_some() & right_index.is_some() {
+                let left_index = left_index.unwrap();
+                let right_index = right_index.unwrap();
+                if left_index >= right_index {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+}
+
+#[derive(Default)]
+pub struct PageOrdering {
+    order_rules: Vec<PageOrderRule>,
+    printings: Vec<Printing>,
+}
+
+impl PageOrdering {
+    fn add_rule_line(&mut self, line: &str) {
         let split = line.trim().split('|').collect::<Vec<_>>();
         if split.len() != 2 {
-            panic!("Rule lines should always be split into 2 - found {} from {}", split.len());
+            panic!("Rule lines should always be split into 2 - found {} from '{}'", split.len(), line);
         }
 
-        self.add_rule(parse_page_number(split[0]), parse_page_number(split[1]));
+        let left = parse_page_number(split[0]);
+        let right = parse_page_number(split[1]);
+
+        self.order_rules.push(PageOrderRule(left, right));
     }
 
-    pub fn add_rule(&mut self, left: u32, right: u32) {
-        let left_index = self.order_rules.iter().find(|&&p| p == left);
-        let right_index = self.order_rules.iter().find(|&&p| p == right);
+    fn add_printing(&mut self, line: &str) {
+        let printing = line.trim()
+            .split(',')
+            .map(|n| parse_page_number(n))
+            .collect::<Printing>();
 
-        if left_index.is_none() && right_index.is_none() {
-            self.order_rules.push(left);
-            self.order_rules.push(right);
-        } else if right_index.is_none() {
-            self.order_rules.push(right);
+        self.printings.push(printing);
+    }
+
+    pub fn check_order_printing(&self) -> (usize, u32) {
+        let mut count = 0;
+        let mut total = 0;
+
+        for printing in self.printings.iter() {
+            if printing.validate(&self.order_rules) {
+                count += 1;
+
+                let mid_point = printing.len() / 2;
+
+                total += printing[mid_point];
+            }
         }
+
+        (count, total)
+    }
+
+}
+
+impl From<&str> for PageOrdering {
+    fn from(value: &str) -> Self {
+        let mut page_ordering = PageOrdering::default();
+
+        let mut completed_rules = false;
+        for line in value.trim().lines() {
+            if line.is_empty() {
+                completed_rules = true;
+            } else if completed_rules {
+                page_ordering.add_printing(line);
+            } else {
+                page_ordering.add_rule_line(line);
+            }
+        }
+
+        page_ordering
     }
 }
 
 fn parse_page_number(n: &str) -> u32 {
     n.parse::<u32>().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &'static str = r"
+    47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+    ";
+
+    #[test]
+    fn valid() {
+        let single = r"
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+        ".trim();
+
+        let page_ordering = PageOrdering::from(single);
+
+        let (valid_count, _) = page_ordering.check_order_printing();
+
+        assert_eq!(1, valid_count);
+    }
+
+    #[test]
+    fn invalid() {
+        let printing: Printing = vec![
+            75,
+            97,
+            47,
+            61,
+            53,
+        ];
+
+        let rules = vec![
+            PageOrderRule(97, 75)
+        ];
+
+        let valid = printing.validate(&rules);
+
+        assert!(!valid);
+    }
+
+    #[test]
+    fn example() {
+        let page_ordering = PageOrdering::from(EXAMPLE);
+
+        let (valid_count, total) = page_ordering.check_order_printing();
+
+        assert_eq!(valid_count, 3);
+
+        assert_eq!(total, 143)
+    }
 }
