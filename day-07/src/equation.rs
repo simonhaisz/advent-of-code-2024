18067a9a8982b798aa4c8a887098c7b9379e2bfc
Simon Haisz <simonhaisz@gmail.com>
2024-12-09 18:31:10 -0500

    day 7 part 1 even without optimization it takes ~30ms to run against the full input

--- day-07/src/equation.rs
+++ day-07/src/equation.rs
@@ -1,31 +1,45 @@
-use crate::operator::Operator;
+use crate::operator::{Operator, OperatorSet};
 
 pub struct Equation {
-    left_hand_value: i32,
-    right_hand_values: Vec<i32>,
+    left_hand_value: u64,
+    right_hand_values: Vec<u64>,
 }
 
 impl Equation {
-    pub fn test(&self, operators: &[Operator]) -> i32 {
-        let mut value = None;
-        let operator_index = 0;
+    pub fn test(&self, operators: &[Operator]) -> u64 {
+        let mut values_it = self.right_hand_values.iter();
+        let mut operators_it = operators.iter();
 
-        for rhv in self.right_hand_values {
-            if value.is_none() {
-                value = Some(rhv);
-            } else {
-                let value = value.as_mut().unwrap();
+        let mut result = *values_it.next().unwrap();
 
-                let operator = operators[operator_index];
+        loop {
+            if let Some(value) = values_it.next() {
+                let operator = operators_it.next().unwrap();
 
-                match operator {
-                    
+                match *operator {
+                    Operator::Add => result += *value,
+                    Operator::Multiply => result *= *value,
                 }
+            } else {
+                break;
             }
         }
+
+        result
     }
+
     pub fn valid(&self) -> bool {
-        unimplemented!()
+        let operator_set = OperatorSet::new((self.right_hand_values.len() - 1) as u32);
+
+        for operators in operator_set {
+            let value = self.test(&operators);
+
+            if value == self.left_hand_value {
+                return true;
+            }
+        }
+
+        false
     }
 
     pub fn possible(&self) -> bool {
@@ -47,14 +61,44 @@ impl From<&str> for Equation {
     fn from(value: &str) -> Self {
         let mut split = value.split(":");
 
-        let left = split.nth(0).unwrap().trim();
-        let right = split.nth(1).unwrap().trim();
+        let left = split.next().unwrap().trim();
+        let right = split.next().unwrap().trim();
 
-        let left_hand_value = left.parse::<i32>().unwrap();
+        let left_hand_value = left.parse::<u64>().unwrap();
 
-        let right_hand_values = right.split(" ").map(|v| v.parse::<i32>().unwrap()).collect::<Vec<_>>();
+        let right_hand_values = right.split(" ").map(|v| v.parse::<u64>().unwrap()).collect::<Vec<_>>();
 
         Self { left_hand_value, right_hand_values }
+    }
+}
+
+pub struct Calibration {
+    equations: Vec<Equation>,
+}
+
+impl Calibration {
+    pub fn result(&self) -> u64 {
+        self.equations.iter()
+            .filter(|e| e.valid())
+            .map(|e| e.left_hand_value)
+            .sum()
+    }
+}
+
+impl From<&str> for Calibration {
+    fn from(text: &str) -> Self {
+        let mut equations = vec![];
+
+        for line in text.lines() {
+            let line = line.trim();
+            if line.is_empty() {
+                continue;
+            }
+            let equation = Equation::from(line);
+            equations.push(equation);
+        }
+    
+        Self { equations }
     }
 }
 
@@ -62,8 +106,84 @@ impl From<&str> for Equation {
 mod tests {
     use super::*;
 
+    const EXAMPLE: &'static str = r"
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+    ";
+
+    #[test]
+    fn example_valid() {
+        let equation = Equation::from("190: 10 19");
+
+        let valid = equation.valid();
+
+        assert!(valid);
+
+        let equation = Equation::from("3267: 81 40 27");
+
+        let valid = equation.valid();
+
+        assert!(valid);
+
+        let equation = Equation::from("292: 11 6 16 20");
+
+        let valid = equation.valid();
+
+        assert!(valid);
+    }
+
+    #[test]
+    fn example_invalid() {
+        let equation = Equation::from("83: 17 5");
+
+        let valid = equation.valid();
+
+        assert!(!valid);
+
+        let equation = Equation::from("156: 15 6");
+
+        let valid = equation.valid();
+
+        assert!(!valid);
+
+        let equation = Equation::from("7290: 6 8 6 15");
+
+        let valid = equation.valid();
+
+        assert!(!valid);
+
+        let equation = Equation::from("161011: 16 10 13");
+
+        let valid = equation.valid();
+
+        assert!(!valid);
+
+        let equation = Equation::from("192: 17 8 14");
+
+        let valid = equation.valid();
+
+        assert!(!valid);
+
+        let equation = Equation::from("21037: 9 7 18 13");
+
+        let valid = equation.valid();
+
+        assert!(!valid);
+    }
+
     #[test]
     fn example() {
+        let calibration = Calibration::from(EXAMPLE);
 
+        let result = calibration.result();
+
+        assert_eq!(result, 3749);
     }
 }
--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,6 +1,30 @@
+use equation::Calibration;
+use utils::{part_selection, Part, Timer};
+
 mod equation;
 mod operator;
 
 fn main() {
-    println!("Hello, world!");
+    let _timer = Timer::new();
+
+    let text = std::fs::read_to_string("./day-07/input.txt").unwrap();
+
+    let part = part_selection();
+
+    match part {
+        Part::One => part_1(&text),
+        Part::Two => part_2(&text),
+    }
+}
+
+fn part_1(input: &str) {
+    let calibration = Calibration::from(input);
+
+    let result = calibration.result();
+
+    println!("{result}");
+}
+
+fn part_2(_input: &str) {
+    unimplemented!();
 }
