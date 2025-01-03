7046989480d8b9e3fcd0e4922dcd0bafefa0c943
Simon Haisz <simonhaisz@gmail.com>
2025-01-03 15:24:56 -0500

    day 16 part 1 laptop WIP transfer

--- day-16/src/maze.rs
+++ day-16/src/maze.rs
@@ -1,6 +1,6 @@
-use std::collections::HashSet;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
-use utils::Grid;
+use utils::{Direction, Grid, Position};
 
 pub struct Maze {
     grid: Grid,
@@ -50,5 +50,66 @@ impl From<&str> for Maze {
         let end_location = end_location.unwrap();
 
         Self { grid, wall_locations, start_location, end_location }
+    }
+}
+
+struct ScoreLocation {
+    location: usize,
+    position: Position,
+    score: u32,
+}
+
+impl ScoreLocation {
+    fn new(location: usize, position: Position, score: u32) -> Self {
+        Self { location, position, score }
+    }
+}
+
+impl PartialEq for ScoreLocation {
+    fn eq(&self, other: &Self) -> bool {
+        self.score == other.score
+    }
+}
+
+impl Eq for ScoreLocation {}
+
+impl PartialOrd for ScoreLocation {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for ScoreLocation {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.score.cmp(&other.score)
+    }
+}
+
+fn find_lowest_score_route(maze: &Maze, start_location: usize, end_location: usize) {
+    let start_position = maze.grid.get_position(start_location).unwrap();
+
+    let mut frontier = BinaryHeap::new();
+    frontier.push(ScoreLocation::new(start_location, start_position, 0));
+
+    let mut came_from = HashMap::new();
+    
+    let mut movement_so_far = HashMap::new();
+    movement_so_far.insert(start_location, 0);
+
+    let mut current_direction = Direction::East;
+
+    while !frontier.is_empty() {
+        let current = frontier.pop().unwrap();
+
+        if current.location == end_location {
+            break;
+        }
+
+        for next_direction in Direction::orthogonal() {
+            let next_position = current.position.adjacent(*next_direction);
+            if let Some(next_location) = maze.grid.get_index(&next_position) {
+                
+            }
+        }
     }
 }
