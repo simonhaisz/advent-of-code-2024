092ee8413e23ffdb398dff4dc6d63f5f17458648
Simon Haisz <simonhaisz@gmail.com>
2025-01-04 14:33:00 -0500

    day 16 part 1 WIP - score too high

--- day-16/src/maze.rs
+++ day-16/src/maze.rs
@@ -9,6 +9,12 @@ pub struct Maze {
     end_location: usize,
 }
 
+impl Maze {
+    pub fn find_lowest_score_route(&self) -> Option<u32> {
+        find_lowest_score_route(self, self.start_location, self.end_location)
+    }
+}
+
 const WALL: char = '#';
 const START: char = 'S';
 const END: char = 'E';
@@ -53,63 +59,146 @@ impl From<&str> for Maze {
     }
 }
 
-struct ScoreLocation {
+#[derive(Clone, Debug)]
+struct Vector {
     location: usize,
     position: Position,
-    score: u32,
+    direction: Direction,
 }
 
-impl ScoreLocation {
-    fn new(location: usize, position: Position, score: u32) -> Self {
-        Self { location, position, score }
+struct ScoreVector {
+    score: u32,
+    vector: Vector,
+}
+
+impl ScoreVector {
+    fn new(location: usize, position: Position, direction: Direction, score: u32) -> Self {
+        let vector = Vector { location, position, direction };
+
+        Self { score, vector }
     }
 }
 
-impl PartialEq for ScoreLocation {
+impl PartialEq for ScoreVector {
     fn eq(&self, other: &Self) -> bool {
         self.score == other.score
     }
 }
 
-impl Eq for ScoreLocation {}
+impl Eq for ScoreVector {}
 
-impl PartialOrd for ScoreLocation {
+impl PartialOrd for ScoreVector {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl Ord for ScoreLocation {
+impl Ord for ScoreVector {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.score.cmp(&other.score)
     }
 }
 
-fn find_lowest_score_route(maze: &Maze, start_location: usize, end_location: usize) {
+fn find_lowest_score_route(maze: &Maze, start_location: usize, end_location: usize) -> Option<u32> {
     let start_position = maze.grid.get_position(start_location).unwrap();
+    let start_direction = Direction::East;
 
     let mut frontier = BinaryHeap::new();
-    frontier.push(ScoreLocation::new(start_location, start_position, 0));
+    frontier.push(ScoreVector::new(start_location, start_position, start_direction, 0));
 
     let mut came_from = HashMap::new();
     
-    let mut movement_so_far = HashMap::new();
-    movement_so_far.insert(start_location, 0);
+    let mut score_so_far = HashMap::new();
+    score_so_far.insert(start_location, 0);
 
-    let mut current_direction = Direction::East;
+    let mut score = None;
 
     while !frontier.is_empty() {
         let current = frontier.pop().unwrap();
 
-        if current.location == end_location {
+        if current.vector.location == end_location {
+            score.replace(current.score);
             break;
         }
 
         for next_direction in Direction::orthogonal() {
-            let next_position = current.position.adjacent(*next_direction);
-            if let Some(next_location) = maze.grid.get_index(&next_position) {
+            let next_position = current.vector.position.adjacent(*next_direction);
+            if let Ok(next_location) = maze.grid.get_index(&next_position) {
+                let rotation = Direction::orthogonal_delta(current.vector.direction, *next_direction) as u32;
+                let next_score = score_so_far[&current.vector.location] + 1 + rotation * 1000;
                 
+                if !score_so_far.contains_key(&next_location) || next_score < score_so_far[&next_location] {
+                    score_so_far.insert(next_location, next_score);
+
+                    let next_score_vector = ScoreVector::new(next_location, next_position, *next_direction, next_score);
+                    frontier.push(next_score_vector);
+
+                    came_from.insert(next_location, current.vector.location);
+                }
             }
         }
+    }
+
+    score
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let text = r"
+###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############
+        ".trim();
+
+        let maze = Maze::from(text);
+
+        let lowest_score = maze.find_lowest_score_route();
+
+        assert_eq!(Some(7036), lowest_score);
+    }
+
+    #[test]
+    fn example_2() {
+        let text = r"
+#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################
+        ".trim();
+
+        let maze = Maze::from(text);
+
+        let lowest_score = maze.find_lowest_score_route();
+
+        assert_eq!(Some(11048), lowest_score);
     }
 }
